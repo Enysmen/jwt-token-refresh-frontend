23f9b070db9205967c87e346f497cfcb8456959d
EsnemyC <120020955+Enysmen@users.noreply.github.com>
2026-01-22 20:18:17 +0200

    Create simple start tokenStorage(auth),add logout fun(authService),and little change code in authApi and authService(exort default)

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -2,10 +2,17 @@ import type {LoginRequestDto } from '../auth/auth.dto';
 import type {LoginResponseDto } from '../auth/auth.dto';
 import { httpClient } from './httpClient';
 
-export  function login(dto:LoginRequestDto): Promise<LoginResponseDto> 
+export const authApi = {
+    login,
+};
+
+
+export function login(dto:LoginRequestDto): Promise<LoginResponseDto> 
 { 
     return httpClient<LoginResponseDto>('/auth/login', {
         method: 'POST',
         body: JSON.stringify(dto),
     });
 }
+
+export default authApi;
