d304cc2cd62c90815269d3a7cb9e95172c752bda
EsnemyC <120020955+Enysmen@users.noreply.github.com>
2026-01-22 18:10:30 +0200

    Create DTO(auth) login Req,Response , create start fun httpclient, create fun simple fun login(authApi)

---
+++ src/api/authApi.ts
@@ -0,0 +1,11 @@
+import type {LoginRequestDto } from '../auth/auth.dto';
+import type {LoginResponseDto } from '../auth/auth.dto';
+import { httpClient } from './httpClient';
+
+export  function login(dto:LoginRequestDto): Promise<LoginResponseDto> 
+{ 
+    return httpClient<LoginResponseDto>('/auth/login', {
+        method: 'POST',
+        body: JSON.stringify(dto),
+    });
+}
